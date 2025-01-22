fb212ba166cb5600ae7965bfc1051aa6d2fb1db9
waitforwork <143585658+waitforwork@users.noreply.github.com>
2025-01-22 20:08:49 +0300

    add 1765

--- tasks.cpp
+++ tasks.cpp
@@ -2748,3 +2748,34 @@ long long tasks::task_2017(std::vector<std::vector<int> > &grid)
     return res;
 }
 
+std::vector<std::vector<int> > tasks::task_1765(std::vector<std::vector<int> > &isWater)
+{
+    const int r=isWater.size(), c=isWater[0].size();
+    const int d[5]={0, 1, 0, -1, 0};
+    std::vector<std::vector<int>> H(r, std::vector<int>(c, -1));
+    std::queue<int> q;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            if (isWater[i][j]){
+                int idx=(i<<10)+j;
+                H[i][j]=0;
+                q.push(idx);
+            }
+        }
+    }
+    for(int h=0; !q.empty(); h++){
+        int qz=q.size();
+        for(int a=0; a<qz; a++){
+            int idx=q.front(), i=idx>>10, j=idx&((1<<10)-1);
+            q.pop();
+            for(int b=0; b<4; b++){
+                int s=i+d[b], t=j+d[b+1];
+                if (s<0||s>=r||t<0||t>=c|| H[s][t]!=-1) continue;
+                q.push((s<<10)+t);
+                H[s][t]=h+1;
+            }
+        }
+    }
+    return H;
+}
+
--- tasks.h
+++ tasks.h
@@ -145,6 +145,7 @@ public:
     int task_407(std::vector<std::vector<int>>& heightMap);                                                         //407. поиск в векторе векторов
     int task_2661(std::vector<int>& arr, std::vector<std::vector<int>>& mat);                                       //2661. поиск в векторах
     long long task_2017(std::vector<std::vector<int>>& grid);                                                       //2017. перестановка в векторе
+    std::vector<std::vector<int>> task_1765(std::vector<std::vector<int>>& isWater);                                //1765. поиск в векторе
 private:
     std::vector<int> result; //501
         int prev = 0;        //501
