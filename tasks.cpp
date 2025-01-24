79921de0c073e049c0d2fa2b212d6e5690d72059
waitforwork <143585658+waitforwork@users.noreply.github.com>
2025-01-24 19:21:23 +0300

    add 802

--- tasks.cpp
+++ tasks.cpp
@@ -2803,3 +2803,40 @@ int tasks::task_1267(std::vector<std::vector<int> > &grid)
     return ans;
 }
 
+std::vector<int> tasks::task_802(std::vector<std::vector<int> > &graph)
+{
+    const int n=graph.size();
+    std::vector<int> indeg(n, 0);
+    std::vector<std::vector<int>> adj(n);
+    for(int i=0; i<n ; i++){
+        for(int j=graph[i].size()-1; j>=0; j--){
+            int v=graph[i][j];
+            adj[v].push_back(i);
+            indeg[i]++;
+        }
+    }
+
+    std::queue<int> q;
+
+    for(int i=n-1; i>=0; i--)
+        if (indeg[i]==0) q.push(i);
+
+    std::vector<bool> safe(n, 0);
+    while(!q.empty()){
+        int x=q.front();
+        //    cout<<x<<"->";;
+        q.pop();
+        safe[x]=1;
+        for(int y: adj[x]){
+            //        cout<<y<<",";
+            if (--indeg[y]==0)
+                q.push(y);
+        }
+        //    cout<<endl;
+    }
+    std::vector<int> ans;
+    for(int i=0; i<n; i++)
+        if (safe[i]) ans.push_back(i);
+    return ans;
+}
+
--- tasks.h
+++ tasks.h
@@ -147,6 +147,7 @@ public:
     long long task_2017(std::vector<std::vector<int>>& grid);                                                       //2017. перестановка в векторе
     std::vector<std::vector<int>> task_1765(std::vector<std::vector<int>>& isWater);                                //1765. поиск в векторе
     int task_1267(std::vector<std::vector<int>>& grid);                                                             //1267. вектор, матрица
+    std::vector<int> task_802(std::vector<std::vector<int>>& graph);                                                //802. поиск в векторе
 private:
     std::vector<int> result; //501
         int prev = 0;        //501
