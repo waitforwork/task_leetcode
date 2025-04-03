f9fe032c3832f1007a64d45e1e8721f080871103
alex <alex198704@mail.ru>
2025-04-03 20:09:56 +0300

    add task_2874

--- tasks.cpp
+++ tasks.cpp
@@ -4608,3 +4608,19 @@ long long tasks::task_2873(std::vector<int> &nums)
     return ans;
 }
 
+long long tasks::task_2874(std::vector<int> &nums)
+{
+    int maxi = INT_MIN, diff = 0, n = nums.size();
+    long long res = 0;
+
+    for (int i = 0; i < n; i++) {
+        maxi = std::max(maxi, nums[i]); // Update the max value seen so far
+        if (i >= 2)
+            res = std::max(res, (long long)diff * nums[i]); // Compute max triplet value
+        if (i >= 1)
+            diff = std::max(diff, maxi - nums[i]); // Update max difference
+    }
+
+    return res;
+}
+
